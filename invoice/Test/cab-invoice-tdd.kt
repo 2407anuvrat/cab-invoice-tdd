d6c26d8143377c5c8e9362aaf23eafbfe2e8ad5e
Anuvrat Karanwal <anuvratkaranwal@Apples-MacBook-Pro-10.local>
2022-05-23 18:31:50 +0530

    implemented logic and testing compatible with passing List of Ride class objects as parameters

--- invoice/Test/cab-invoice-tdd.kt
+++ invoice/Test/cab-invoice-tdd.kt
@@ -5,19 +5,23 @@ import io.kotest.matchers.shouldBe
 class CabInvoiceTest : StringSpec() {
     init {
         "fare of 1 km covered in 0 min should be Rs 10" {
-            CabInvoice().fare(Ride(1.0, 0.0)) shouldBe 10.0
+            CabInvoice().fare(listOf( Ride(1.0, 0.0))) shouldBe 10.0
         }
 
         "fare of 0 km covered in 10 min should be Rs 10" {
-            CabInvoice().fare(Ride(0.0, 10.0)) shouldBe 10.0
+            CabInvoice().fare(listOf( Ride(0.0, 10.0))) shouldBe 10.0
         }
 
         "fare of 1.2 km covered in 5.5 min should be Rs 10" {
-            CabInvoice().fare(Ride(1.2, 5.5)) shouldBe 17.5
+            CabInvoice().fare(listOf( Ride(1.2, 5.5))) shouldBe 17.5
         }
 
         "fare of given array of distance and time should be 12.3"{
-            CabInvoice().fare(Ride(1.0, 2.3)) shouldBe 12.3
+            CabInvoice().fare(listOf( Ride(1.0, 2.3))) shouldBe 12.3
+        }
+
+        "fare of multiple ride object is implemented"{
+            CabInvoice().fare(listOf( Ride(1.0, 2.3), Ride(2.0, 4.9), Ride(10.0, 0.0), Ride(0.0, 20.0))) shouldBe 157.2
         }
     }
 }
--- invoice/src/cab-invoice.kt
+++ invoice/src/cab-invoice.kt
@@ -5,10 +5,14 @@ class CabInvoice {
         val fare_per_unit_time = 1.0
     }
 
-    fun fare(rides: Ride): Double {
+    fun fare(rides: List<Ride>): Double {
 
-
-        val (dist, time) = rides
+        var dist: Double = 0.0
+        var time: Double = 0.0
+        for (ride in rides){
+            dist += ride.dist
+            time += ride.time
+        }
         return dist * fare_per_unit_dist + time * fare_per_unit_time
     }
 }
