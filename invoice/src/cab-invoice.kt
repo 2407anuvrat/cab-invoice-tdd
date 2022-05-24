dcc3bdfd99b9be849a1e31fa6cdb2b791a9a579b
Anuvrat Karanwal <anuvratkaranwal@Apples-MacBook-Pro-10.local>
2022-05-24 12:50:57 +0530

    implemented code preventing Feature Envy code smell

--- invoice/src/cab-invoice.kt
+++ invoice/src/cab-invoice.kt
@@ -1,13 +1,6 @@
 class CabInvoice {
 
-    companion object {
-        val fare_per_unit_dist =  10.0
-        val fare_per_unit_time = 1.0
-    }
-
     fun fare(rides: List<Ride>): Double {
-
-        return rides.sumOf { it.dist*fare_per_unit_dist + it.time*fare_per_unit_time }
-
+        return rides.sumOf { it.calculateFare() }
     }
 }
--- invoice/src/ride.kt
+++ invoice/src/ride.kt
@@ -1,2 +1,11 @@
 
-data class Ride(val dist: Double, val time: Double)
+data class Ride(val dist: Double, val time: Double) {
+
+    companion object {
+        val fare_per_unit_dist =  10.0
+        val fare_per_unit_time = 1.0
+    }
+    fun calculateFare() : Double{
+        return this.dist * fare_per_unit_dist + this.time * fare_per_unit_time
+    }
+}
